3050b66c26dd2171a8f0813314040f4b944fe2b0
Erik Karlsson <erik.karlsson.6@volvocars.com>
2023-06-14 08:54:02 +0200

    don't know

--- src/turn_tracker.rs
+++ src/turn_tracker.rs
@@ -6,14 +6,14 @@ use log::debug;
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct TurnTracker {
     players: Vec<User>,
-    current_player_index: usize,
+    next_player_index: usize,
 }
 
 impl TurnTracker {
     fn player_string(&self) -> String {
         let mut players: String = String::new();
         for (i, User { name, .. }) in enumerate(&self.players) {
-            if i == self.current_player_index {
+            if i == self.next_player_index {
                 players += format!("*{name}").as_str();
             } else {
                 players += name.to_string().as_str();
@@ -26,7 +26,7 @@ impl TurnTracker {
         debug!("Creating turn tracker, with users {players:?}");
         Self {
             players,
-            current_player_index: 0,
+            next_player_index: 0,
         }
     }
 
@@ -39,14 +39,12 @@ impl TurnTracker {
             .find_position(|u| u.name == username)
             .unwrap();
 
-        if i <= self.current_player_index {
+        if i <= self.next_player_index {
             // Remove player earlier in the list
-            if self.players.is_empty() {
-                self.current_player_index = 0;
-            } else if i == 0 {
-                self.current_player_index = self.players.len() - 1;
-            } else {
-                self.current_player_index -= 1;
+            if i < self.next_player_index {
+                self.next_player_index -= 1;
+            } else if self.next_player_index == self.players.len() - 1 {
+                self.next_player_index = 0;
             }
         }
         self.players = self
@@ -69,13 +67,134 @@ impl TurnTracker {
         if self.players.is_empty() {
             return None;
         }
-        self.current_player_index = (self.current_player_index + 1) % self.players.len();
-        self.players
-            .get(self.current_player_index)
-            .map(Clone::clone)
+        let current_index = self.next_player_index;
+        self.next_player_index = (self.next_player_index + 1) % self.players.len();
+        self.players.get(current_index).map(Clone::clone)
     }
 
     pub fn num_players(&self) -> usize {
         self.players.len()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn make_player(name: &str) -> User {
+        User {
+            name: name.to_string(),
+            color: druid::piet::Color::BLUE,
+        }
+    }
+
+    #[test]
+    fn construct_and_loop() {
+        let p1 = make_player("p1");
+        let p2 = make_player("p2");
+        let p3 = make_player("p3");
+        let mut t = TurnTracker::new(vec![p1.clone(), p2.clone(), p3.clone()]);
+
+        for _ in 1..50 {
+            assert_eq!(t.advance_player(), Some(p1.clone()));
+            assert_eq!(t.advance_player(), Some(p2.clone()));
+            assert_eq!(t.advance_player(), Some(p3.clone()));
+        }
+    }
+
+    #[test]
+    fn add_one() {
+        let p1 = make_player("p1");
+        let p2 = make_player("p2");
+        let mut t = TurnTracker::new(vec![p1.clone(), p2.clone()]);
+
+        assert_eq!(t.advance_player(), Some(p1.clone()));
+        let p3 = make_player("p3");
+        t.add_player(p3.clone());
+        for _ in 1..10 {
+            assert_eq!(t.advance_player(), Some(p2.clone()));
+            assert_eq!(t.advance_player(), Some(p3.clone()));
+            assert_eq!(t.advance_player(), Some(p1.clone()));
+        }
+    }
+
+    #[test]
+    fn add_two() {
+        let p1 = make_player("p1");
+        let mut t = TurnTracker::new(vec![p1.clone()]);
+
+        assert_eq!(t.advance_player(), Some(p1.clone()));
+        let p2 = make_player("p2");
+        let p3 = make_player("p3");
+        t.add_player(p2.clone());
+        t.add_player(p3.clone());
+        for _ in 1..10 {
+            assert_eq!(t.advance_player(), Some(p1.clone()));
+            assert_eq!(t.advance_player(), Some(p2.clone()));
+            assert_eq!(t.advance_player(), Some(p3.clone()));
+        }
+    }
+    #[test]
+    fn remove_last() {
+        let p1 = make_player("p1");
+        let p2 = make_player("p2");
+        let mut t = TurnTracker::new(vec![p1.clone(), p2.clone()]);
+
+        assert_eq!(t.advance_player(), Some(p1.clone()));
+        t.remove_player("p1");
+        for _ in 1..10 {
+            assert_eq!(t.advance_player(), Some(p2.clone()));
+        }
+    }
+
+    #[test]
+    fn remove_next() {
+        let p1 = make_player("p1");
+        let p2 = make_player("p2");
+        let mut t = TurnTracker::new(vec![p1.clone(), p2.clone()]);
+
+        assert_eq!(t.advance_player(), Some(p1.clone()));
+        t.remove_player("p2");
+        for _ in 1..10 {
+            assert_eq!(t.advance_player(), Some(p1.clone()));
+        }
+    }
+
+    #[test]
+    fn remove_all() {
+        let p1 = make_player("p1");
+        let p2 = make_player("p2");
+        let mut t = TurnTracker::new(vec![p1.clone(), p2.clone()]);
+
+        assert_eq!(t.advance_player(), Some(p1.clone()));
+        t.remove_player("p2");
+        t.remove_player("p1");
+        for _ in 1..10 {
+            assert_eq!(t.advance_player(), None);
+        }
+    }
+
+    #[test]
+    fn start_empty() {
+        let p1 = make_player("p1");
+        let p2 = make_player("p2");
+        let mut t = TurnTracker::new(vec![]);
+
+        assert_eq!(t.advance_player(), None);
+        t.add_player(p1.clone());
+        t.add_player(p2.clone());
+        for _ in 1..10 {
+            assert_eq!(t.advance_player(), Some(p1.clone()));
+            assert_eq!(t.advance_player(), Some(p2.clone()));
+        }
+    }
+
+    #[test]
+    fn single_player() {
+        let p1 = make_player("p1");
+        let mut t = TurnTracker::new(vec![p1.clone()]);
+        for _ in 1..10 {
+            assert_eq!(t.advance_player(), Some(p1.clone()));
+        }
     }
 }
